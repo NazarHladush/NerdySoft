1ac4dca58a7a1626e2c6bf5f6ba562a280b05367
Nazar Hladysh <nazar.gladish@gmail.com>
2020-07-06 18:23:11 +0300

    generated structure

---
+++ src/main/java/com/nerdysoft/NerdySoftApplication.java
@@ -0,0 +1,13 @@
+package com.nerdysoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdySoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdySoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nerdysoft/NerdySoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdysoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdySoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
