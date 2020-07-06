167a9e4c724f84aa7ef2346d6c365b6da76c78e7
Nazar Hladysh <nazar.gladish@gmail.com>
2020-07-06 20:11:25 +0300

    add Role entity

---
+++ src/main/java/com/nerdysoft/model/Role.java
@@ -0,0 +1,25 @@
+package com.nerdysoft.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role", length = 25, nullable = false)
+    private String role;
+
+}
