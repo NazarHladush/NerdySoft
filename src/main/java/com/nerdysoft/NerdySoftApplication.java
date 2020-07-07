076e93eb0aa4ebc5fbcf325c8c054c692621929f
Nazar Hladysh <nazar.gladish@gmail.com>
2020-07-07 10:19:31 +0300

    add default user and roles to DB

--- src/main/java/com/nerdysoft/NerdySoftApplication.java
+++ src/main/java/com/nerdysoft/NerdySoftApplication.java
@@ -1,7 +1,18 @@
 package com.nerdysoft;
 
+import com.nerdysoft.model.Role;
+import com.nerdysoft.model.User;
+import com.nerdysoft.model.UserStatus;
+import com.nerdysoft.repository.RoleRepository;
+import com.nerdysoft.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collections;
+import java.util.HashSet;
 
 @SpringBootApplication
 public class NerdySoftApplication {
@@ -10,4 +21,22 @@ public class NerdySoftApplication {
 		SpringApplication.run(NerdySoftApplication.class, args);
 	}
 
+	@Bean
+    public CommandLineRunner commandLineRunner(RoleRepository roleRepository,
+											   UserRepository userRepository,
+											   PasswordEncoder passwordEncoder) {
+        return args -> {
+			roleRepository.save(Role.builder().role("USER").build());
+			roleRepository.save(Role.builder().role("ADMIN").build());
+			userRepository.save(User.builder()
+					.email("nazar.gladish@gmail.com")
+					.firstName("Nazar")
+					.lastName("Hladysh")
+					.password(passwordEncoder.encode("nazar12345"))
+					.status(UserStatus.ACTIVATED)
+					.roles(new HashSet<>(Collections.singletonList(roleRepository.findByRole("USER"))))
+					.build()
+			);
+		};
+    }
 }
