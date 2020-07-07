258cdceb2eb538142edfaaecf15162a5777afe4c
Nazar Hladysh <nazar.gladish@gmail.com>
2020-07-07 10:03:57 +0300

    add dto for registration

---
+++ src/main/java/com/nerdysoft/model/UserRegistrationDto.java
@@ -0,0 +1,20 @@
+package com.nerdysoft.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationDto {
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+}
