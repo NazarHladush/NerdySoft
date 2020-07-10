7aa69f48a5e641cf6dad8e8cd6ba3d139c7998db
Nazar Hladysh <nazar.gladish@gmail.com>
2020-07-10 18:31:51 +0300

    add creating default roles

--- src/main/java/com/nerdysoft/NerdySoftApplication.java
+++ src/main/java/com/nerdysoft/NerdySoftApplication.java
@@ -1,9 +1,13 @@
 package com.nerdysoft;
 
 import com.nerdysoft.config.JwtConfig;
+import com.nerdysoft.model.Role;
+import com.nerdysoft.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtConfig.class)
@@ -13,5 +17,12 @@ public class NerdySoftApplication {
         SpringApplication.run(NerdySoftApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+        return args -> {
+            roleRepository.save(Role.builder().role("USER").build());
+            roleRepository.save(Role.builder().role("ADMIN").build());
 
+        };
+    }
 }
