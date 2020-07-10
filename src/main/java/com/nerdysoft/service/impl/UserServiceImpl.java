0187783ad6c21e9a5f21e189e95f1a9b6ef929d5
Nazar Hladysh <nazar.gladish@gmail.com>
2020-07-10 17:54:00 +0300

    change mail sender credentials

--- src/main/java/com/nerdysoft/service/impl/UserServiceImpl.java
+++ src/main/java/com/nerdysoft/service/impl/UserServiceImpl.java
@@ -67,7 +67,7 @@ public class UserServiceImpl implements UserService {
         userActivationRequestRepository.save(userActivationRequest);
 
         String message = "Welcome. To activate your account follow link:  "
-                + "http://localhost:8080/api/v1/registration/activationUser?activationCode="
+                + "https://nerdysoftjava.herokuapp.com/api/v1/activationUser?activationCode="
                 + userActivationRequest.getActivationCode();
 
         mailSender.send(user.getEmail(), "Activation Code", message);
