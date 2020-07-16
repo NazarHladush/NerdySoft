70c8926f1f8a4072ed10c92fd9b85b8f3364a858
Nazar Hladysh <nazar.gladish@gmail.com>
2020-07-16 10:50:20 +0300

    little fix

--- src/main/java/com/nerdysoft/service/impl/UserServiceImpl.java
+++ src/main/java/com/nerdysoft/service/impl/UserServiceImpl.java
@@ -67,7 +67,7 @@ public class UserServiceImpl implements UserService {
         userActivationRequestRepository.save(userActivationRequest);
 
         String message = "Welcome. To activate your account follow link:  "
-                + "https://https://nerdysoftclient.herokuapp.com/activationUser/"
+                + "https://nerdysoftclient.herokuapp.com/activationUser/"
                 + userActivationRequest.getActivationCode();
 
         mailSender.send(user.getEmail(), "Activation Code", message);
